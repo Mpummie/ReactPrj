882600ae457ccf74abb3716328c86978686c36b2
Nompumelelo <128611034+Mpummie@users.noreply.github.com>
2023-06-05 08:03:40 +0200

    Files committed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+
+function App(){
+  return(
+
+    <div className='App'>
+      <Pokemon/>
+    </div>
+
+  );
+}
+
+export default App;
+
+//Names of Pokemon to search for
+//Pikachu, charizard, Lucario, Ditto, piplup, Mewtwo, Charmander, Bulbasaur, Gengar,Torterra, e.t.c
